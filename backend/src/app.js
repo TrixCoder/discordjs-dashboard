c6ef458291be500650d0a4d050a98c0502f7c745
Utkarsh Kumar <utk.bkkg@gmail.com>
2021-05-30 16:19:02 +0530

    Login auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,30 @@
 require('dotenv').config();
+require('./strategies/discord')
 const express = require('express');
+const passport = require('passport');
 const app = express();
 const PORT = process.env.PORT || 3002;
 const routes = require('./routes');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const Store = require('connect-mongo')(session);
 
+mongoose.connect('mongodb://localhost/dashy',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(session({
+    secret: 'secret',
+    cookie:{
+        maxAge: 60000*60*24
+    },
+    resave: false,
+    saveUninitialized:false,
+    store: new Store({mongooseConnection:mongoose.connection})
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/api', routes);
 
 app.listen(PORT, ()=> console.log(`Running on port ${PORT}`));
--- backend/src/strategies/discord.js
+++ backend/src/strategies/discord.js
@@ -1,12 +1,47 @@
 const passport = require('passport')
 const DiscordStrategy = require('passport-discord')
+const User = require('./../database/schemas/User')
 
+passport.serializeUser((user, done) => {
+    done(null, user.discordId)
+});
+passport.deserializeUser(async (discordId, done) =>{
+    try{
+        const user = await User.findOne({discordId});
+        return user ? done(null, user): done(null,null);
+    }catch(err){
+        done(err,null);
+    }
+})
 passport.use(new DiscordStrategy({
     clientID: process.env.DASHBOARD_CLIENT_ID,
     clientSecret: process.env.DASHBOARD_CLIENT_SECRET,
     callbackURL: process.env.DASHBOARD_CALLBACK_URL,
     scope: ['identify', 'guilds'],
-}, async(accessToken, refreshToken, profile, done) = {
-
+}, async (accessToken, refreshToken, profile, done) => {
+    const { id, username, discriminator, avatar, guilds } = profile;
+    console.log(id, username, discriminator, avatar, guilds);
+    try {
+        const findUser = await User.findOneAndUpdate({ discordId: id }, {
+            discordTag: `${username}#${discriminator}`,
+            avatar,
+            guilds
+        }, { new: true })
+        if (findUser) {
+            console.log('User was not found');
+            return done(null, findUser)
+        } else {
+            const newUser = await User.create({
+                discordId: id,
+                discordTag: `${username}#${discriminator}`,
+                avatar,
+                guilds
+            });
+            return done(null, newUser);
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
 })
 );
