1267539ada5eacc50580dd952fb1c119539bc1bf
Utkarsh Kumar <utk.bkkg@gmail.com>
2021-06-01 12:15:25 +0530

    Guild Dashboard issue fixed

--- dashboard-client/src/App.js
+++ dashboard-client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
-import {LandingPage, MenuPage, DashboardPage} from './pages'
+import { LandingPage, MenuPage, DashboardPage } from './pages'
 
 function App() {
   return (
     <Switch>
-      <Route path= "/" exact={true} component={LandingPage} />
-      <Route path= "/menu" exact={true} component={MenuPage} />
-      <Route path="/dashboard:id" exact={true} component={DashboardPage}/>
+      <Route path="/" exact={true} component={LandingPage} />
+      <Route path="/menu" exact={true} component={MenuPage} />
+      <Route path="/dashboard/:id" exact={true} component={DashboardPage} />
     </Switch>
   );
 }
--- dashboard-client/src/pages/DashboardPage/index.jsx
+++ dashboard-client/src/pages/DashboardPage/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { getUserDetails } from '../../utils/api';
+import { Formik } from 'formik';
+import { Input, Button } from '@chakra-ui/react'
 
 export function DashboardPage({
     history,
 }) {
     const [user, setUser] = React.useState(null);
     const [loading, setLoading] = React.useState(true);
+    const [prefix, setPrefix] = React.useState('!');
 
     React.useEffect(() => {
         getUserDetails()
@@ -21,6 +24,20 @@ export function DashboardPage({
     return !loading && (
         <div>
             <h1>Dashboard Page</h1>
+            <Formik
+                initialValues={{ prefix }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                {
+                    (props) => (
+                        <form onSubmit={props.handleSubmit}>
+                            <Input type="text" name="prefix" onChange={props.handleChange} defaultValue={prefix} />
+                            <Button type="submit" colorScheme="orange" children="Update Prefix" />
+                        </form>
+                    )}
+            </Formik>
         </div>
     )
 
